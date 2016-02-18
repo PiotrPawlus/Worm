e7e9eff68bb2b4fa4f6b1a8fcf8be38e8a8b9448
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-18 11:14:45 +0100

    #2 add starting button

--- Worm/GameScene.swift
+++ Worm/GameScene.swift
@@ -9,10 +9,15 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    private var playButton: SKButton!
+    private let buttonsScale: CGFloat = 0.8
+
     // MARK: - Presenting a Scene
     override func didMoveToView(view: SKView) {
         
         self.background()
+        self.setPlayButton()
 
     }
     
@@ -33,5 +38,17 @@ class GameScene: SKScene {
         backgorundSprite.size = self.frame.size
         backgorundSprite.position = CGPointMake(self.frame.midX, self.frame.midY)
         self.addChild(backgorundSprite)
+    }
+    
+    func setPlayButton() {
+        playButton = SKButton(defaultButtonImage: "PlayBigButton", activeButtonImage: "PlayBigButton", buttonAction: removePlayButton)
+        playButton.setScale(self.buttonsScale)
+        playButton.zPosition = ObjectsZPositions.hud
+        playButton.position = CGPointMake(self.frame.midX, self.frame.midY)
+        self.addChild(playButton)
+    }
+    
+    func removePlayButton() {
+        playButton.removeFromParent()
     }
 }
