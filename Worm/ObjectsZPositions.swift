7e4989ff2484e8c37590b1ebf9d1116463a3031c
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-18 08:38:12 +0100

    set app just in portrait orientation, add ObjectsZPositions struct

---
+++ Worm/ObjectsZPositions.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Worm
+//
+//  Created by Piotr Pawluś on 18/02/16.
+//  Copyright © 2016 Piotr Pawluś. All rights reserved.
+//
+
+import UIKit
+
+struct ObjectsZPositions {
+    static let background = -2
+    static let middleground = 1
+    static let foreground = 2
+    static let hud = 3
+}
