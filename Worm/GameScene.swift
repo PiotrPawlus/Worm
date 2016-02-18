b9393a7421f840169787228f3fa010a7164b61c7
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-18 10:56:06 +0100

    #1 add Game Scene

---
+++ Worm/GameScene.swift
@@ -0,0 +1,37 @@
+//
+//  GameScene.swift
+//  Worm
+//
+//  Created by Piotr Pawluś on 18/02/16.
+//  Copyright © 2016 Piotr Pawluś. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    // MARK: - Presenting a Scene
+    override func didMoveToView(view: SKView) {
+        
+        self.background()
+
+    }
+    
+    
+    // MARK: - Responding to Touch Events
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    }
+    
+    // MARK: - Executing the Animation Loop
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+    
+    // MARK: - MenuScene objects
+    func background() {
+        let backgorundSprite = SKSpriteNode(imageNamed: "background2")
+        backgorundSprite.zPosition = ObjectsZPositions.background
+        backgorundSprite.size = self.frame.size
+        backgorundSprite.position = CGPointMake(self.frame.midX, self.frame.midY)
+        self.addChild(backgorundSprite)
+    }
+}
