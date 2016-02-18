1e000a2b722f3541bcd9370a7d3f76742dd50a09
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-18 08:54:54 +0100

    add background, new background assets

--- Worm/MenuScene.swift
+++ Worm/MenuScene.swift
@@ -12,39 +12,30 @@ class MenuScene: SKScene {
     
     // MARK: - Presenting a Scene
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+
+        self.background()
     }
     
     
     // MARK: - Responding to Touch Events
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Worm")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
  
     // MARK: - Executing the Animation Loop
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    // MARK: - MenuScene objects
+    func background() {
+        let backgorundSprite = SKSpriteNode(imageNamed: "background2")
+        backgorundSprite.zPosition = ObjectsZPositions.background
+        backgorundSprite.size = self.frame.size
+        backgorundSprite.position = CGPointMake(self.frame.midX, self.frame.midY)
+        self.addChild(backgorundSprite)
+    }
+    
+    func bigPlayButton() {
+        
     }
 }
