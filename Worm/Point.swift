67342a6970e8ac3764d0dee2469e14014fe051d2
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-19 13:23:05 +0100

    #2 move create point from func to class

---
+++ Worm/Point.swift
@@ -0,0 +1,34 @@
+//
+//  Point.swift
+//  Worm
+//
+//  Created by Piotr Pawluś on 19/02/16.
+//  Copyright © 2016 Piotr Pawluś. All rights reserved.
+//
+
+import SpriteKit
+
+class Point: SKSpriteNode {
+    private weak var delegate: GameScene!
+    
+    // MARK: - initializers
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(imageNamed: String, delegate: GameScene) {
+        let texture = SKTexture(imageNamed: imageNamed)
+        self.delegate = delegate
+        let pointSzie = texture.size()
+        super.init(texture: texture, color: UIColor.clearColor(), size: pointSzie)
+        self.setScale(0.5)
+        
+        self.zPosition = ObjectsZPositions.middleground
+        self.physicsBody = SKPhysicsBody(rectangleOfSize: self.size)
+        self.physicsBody?.dynamic = false
+        self.physicsBody?.categoryBitMask = CollisionCategoryBitmask.Point
+        self.physicsBody?.contactTestBitMask = CollisionCategoryBitmask.Nil
+        self.physicsBody?.collisionBitMask = CollisionCategoryBitmask.Worm
+
+    }
+}
