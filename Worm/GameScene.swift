b13d9f19f16c91f2a9a0c3808f8efebeaae59a3e
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-19 09:52:57 +0100

    #2 add worm sprite with physics body - player

--- Worm/GameScene.swift
+++ Worm/GameScene.swift
@@ -12,6 +12,12 @@ class GameScene: SKScene {
     
     private var playButton: SKButton!
     private let buttonsScale: CGFloat = 0.8
+    private var pointsLabel: PointsNode!
+    
+    // Physics Bodies
+    var worm: SKSpriteNode!
+    var walls: [SKNode]!
+    var point: SKSpriteNode!
 
     // MARK: - Presenting a Scene
     override func didMoveToView(view: SKView) {
@@ -19,6 +25,12 @@ class GameScene: SKScene {
         self.background()
         self.setPlayButton()
         self.setPauseButton()
+        pointsLabel = PointsNode(imageNamed: "Points", frameSize: self.frame.size, delegate: self)
+        pointsLabel.position = CGPointMake(self.frame.maxX * 15/16 - pointsLabel.size.width / 2, self.frame.maxY * 15/16)
+        self.addChild(pointsLabel)
+        
+        // Physics Bodies
+        self.createWorm()
     }
     
     
@@ -28,10 +40,10 @@ class GameScene: SKScene {
     
     // MARK: - Executing the Animation Loop
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        pointsLabel.pointLabel.text = "1"
     }
     
-    // MARK: - MenuScene objects
+    // MARK: - GameScene Sprite Nodes
     func background() {
         let backgorundSprite = SKSpriteNode(imageNamed: "background2")
         backgorundSprite.zPosition = ObjectsZPositions.background
@@ -54,10 +66,32 @@ class GameScene: SKScene {
         pauseButton.position = CGPointMake(self.frame.maxX * 1/10, self.frame.maxY * 15/16)
         self.addChild(pauseButton)
     }
- 
+    
+    // MARK: - GameScene Physic Bodies
+    func createWorm() {
+        worm = SKSpriteNode(imageNamed: "Worm")
+        worm.position = CGPointMake(self.frame.midX, self.frame.midY)
+        worm.zPosition = ObjectsZPositions.middleground
+        worm.setScale(0.5) // to delete, replace with new worm sprite
+        
+        worm.physicsBody = SKPhysicsBody(rectangleOfSize: worm.size)
+        worm.physicsBody?.dynamic = false // at start
+        worm.physicsBody?.allowsRotation = true
+        worm.physicsBody?.affectedByGravity = false
+        
+        worm.physicsBody?.usesPreciseCollisionDetection = true
+        worm.physicsBody?.categoryBitMask = CollisionCategoryBitmask.Worm
+        worm.physicsBody?.collisionBitMask = 0x00
+        worm.physicsBody?.contactTestBitMask = CollisionCategoryBitmask.Wall | CollisionCategoryBitmask.Point
+        self.addChild(worm)
+    }
+    
+    
     // MARK: - Buttons actions
     func removePlayButton() {
         playButton.removeFromParent()
+        worm.physicsBody?.affectedByGravity = true // to delete
+        worm.physicsBody?.dynamic = true
     }
     
     func pauseGame() {
