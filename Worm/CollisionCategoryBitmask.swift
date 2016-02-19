9da100075699e2e7f84b50ffe589b81d6ba37478
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-19 09:44:53 +0100

    #2 add collision category bitmask

---
+++ Worm/CollisionCategoryBitmask.swift
@@ -0,0 +1,15 @@
+//
+//  CollisionCategoryBitmask.swift
+//  Worm
+//
+//  Created by Piotr Pawluś on 19/02/16.
+//  Copyright © 2016 Piotr Pawluś. All rights reserved.
+//
+
+import UIKit
+
+struct CollsionCategoryBitmask {
+    static let Worm: UInt32 = 0x00
+    static let Wall: UInt32 = 0x01
+    static let Point: UInt32 = 0x02
+}
