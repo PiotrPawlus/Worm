3dae892756ddcd60207d82f3d44c05baaef73178
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-02-25 13:57:15 +0100

    create MultiPlayer GameScene

---
+++ Worm/GameSceneMP.swift
@@ -0,0 +1,13 @@
+//
+//  GameSceneMP.swift
+//  Worm
+//
+//  Created by Piotr Pawluś on 25/02/16.
+//  Copyright © 2016 Piotr Pawluś. All rights reserved.
+//
+
+import UIKit
+
+class GameSceneMP: SKScene {
+
+}
